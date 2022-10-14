f31dc9f29b71033be3b607728fcb9d6cbe53d2bf
Admin <Admin@TEB123012>
2022-10-14 08:49:52 +0200

    Add project files.

---
+++ Kapitel 10 uppgift 3/Kapitel_10_uppgift_3.py
@@ -0,0 +1,10 @@
+
+def maximum(a,b):
+    if a<b:
+        print("Storsta tal: ",b)
+    elif a>b:
+        print("Storsta tal: ",a)
+
+tal1=input("Tal 1: ")
+tal2=input("Tal 2: ")
+maximum(tal1,tal2)
